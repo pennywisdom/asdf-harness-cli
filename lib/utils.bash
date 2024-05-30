d6f3efdb423242eab22bfe8d1f616d39464b6e6c
pennywisdom <pennywisdom@users.noreply.github.com>
2024-05-30 11:25:25 +0100

    WIP: wip

--- lib/utils.bash
+++ lib/utils.bash
@@ -74,6 +74,8 @@ install_version() {
 	local version="$2"
 	local install_path="${3%/bin}/bin"
 
+	echo "installing version ${version} to ${install_path} ..."
+
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
 	fi
